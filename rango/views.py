dd42e0b009da7cc4f7d2fc7daee3fe221c442ba8
Tom Hossell <2771954H@student.gla.ac.uk>
2024-01-29 17:14:52 +0000

    Chapter 4 done, though media not working

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    #Construct dictionary to pass the template engine as its context.
+    #Note the key boldmessage matches to {{ boldmessage  }} in the template!
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    
+    #Return a rendered response to send to the client
+    #We make use of the shortcut function to make our lives easier
+    #Note that the first parameter is the template we wish to use
+    return render(request, 'rango/index.html', context = context_dict)
+
+def about(request):
+    context_dict = {'boldmessage' : """Rango was created in 2011 and is considered a original take on both the western and animated genres. \n This tutorial has been
+                    put together by Tom Hossell."""}
+    
+    return render(request, 'rango/about.html', context = context_dict)
+
